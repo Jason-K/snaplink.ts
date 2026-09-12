156477a0fe78e8545e7a997d1d8a493a068daeb2
Jason-K <80914358+Jason-K@users.noreply.github.com>
2026-09-11 21:01:25 -0700

    feat: update simultaneous bindings to use hold action for better user experience

--- src/definitions/simultaneous.ts
+++ src/definitions/simultaneous.ts
@@ -1,13 +1,13 @@
 import { APPS, CMDS, URLS } from "../data";
-import { bind, key, press, simultaneous, to, type Binding } from "../engine";
+import { bind, hold, key, press, simultaneous, to, type Binding } from "../engine";
 
 export const simultaneousBindings: Binding[] = [
   // Examples:
   // bind(simultaneous("j", "k"), to(press(key("escape")))),
   // bind(chord("s", "w"), to(hold(url("cleanshot://capture-window?action=copy&pin")))),
   bind(simultaneous("left_option", "right_option"), to(press(key("slash", ["right_control"])))),
-  bind(simultaneous("e", "k", 30).uninterrupted(), to(press(APPS.keSettings))),
-  bind(simultaneous("e", "v", 30).uninterrupted(), to(press(APPS.keEvents))),
-  bind(simultaneous("q", "r", 30).uninterrupted(), to(press(CMDS.recentAdditions))),
-  bind(simultaneous("r", "w", 30).uninterrupted(), to(press(URLS.rayRecentDocs))),
+  bind(simultaneous("e", "k", 30).uninterrupted(), to(hold(APPS.keSettings))),
+  bind(simultaneous("e", "v", 30).uninterrupted(), to(hold(APPS.keEvents))),
+  bind(simultaneous("q", "r", 30).uninterrupted(), to(hold(CMDS.recentAdditions))),
+  bind(simultaneous("r", "w", 30).uninterrupted(), to(hold(URLS.rayRecentDocs))),
 ];
